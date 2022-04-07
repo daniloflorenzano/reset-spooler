c8088f2b6c2341f9c1badfa2aabe4650d53ee61d
Danilo Florenzano <daniloflorenzano1@outlook.com>
2022-04-07 19:36:45 -0300

    subindo projeto

---
+++ Resetar-Impressao.ps1
@@ -0,0 +1,16 @@
+﻿$wshell = New-Object -ComObject Wscript.Shell
+$wshell = New-Object -ComObject Wscript.Shell
+$printersFolder = "C:\Windows\System32\spool\PRINTERS\*"
+
+function CleanFolder {
+    param ($Folder)
+
+    Remove-Item $Folder
+}
+
+Stop-Service -Name Spooler -Force
+CleanFolder($printersFolder)
+Start-Service -Name Spooler
+
+$wshell.Popup("Serviço de impressão reiniciado. Caso o problema de comunicação com a impressora persista, contate a TI no ramal 204.",0,"Done",0x1)
+
